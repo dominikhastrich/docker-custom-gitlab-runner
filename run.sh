d52428c50ad5769cae03e8232888ffe49c5b282b
Dominik Hastrich <dominik.hastrich@gmail.com>
2015-09-01 15:47:43 +0200

    Optimize monitoring

--- run.sh
+++ run.sh
@@ -26,10 +26,12 @@ cat /etc/gitlab-runner/config.toml
 
 echo ""
 echo "Firing up GitLab CI runner"
-gitlab-ci-multi-runner start
+gitlab-ci-multi-runner start &
 echo "status"
 gitlab-ci-multi-runner verify
 
 echo ""
 echo "monitor service log"
+# create log file (if not existing)
+touch /output/cround-services.log
 tail -f /output/cround-services.log
