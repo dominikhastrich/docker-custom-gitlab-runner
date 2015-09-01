f47575957d4845f12fa6959bb0897b8135831655
Dominik Hastrich <dominik.hastrich@gmail.com>
2015-09-01 11:02:55 +0200

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,27 @@
-# initiate ssh key
-openssl enc -base64 -d <<< $CI_SSH > ~/.ssh/id_rsa
-openssl enc -base64 -d <<< $CI_SSH_PUB > ~/.ssh/id_rsa.pub
+echo ""
+echo "set ssh key"
+# Write env vars to disc for decoding
+echo $CI_SSH > /tmp/CI_SSH
+echo $CI_SSH_PUB > /tmp/CI_SSH_PUB
+# create ssh dir (if not there)
+mkdir -p ~/.ssh
+# decode tmp files
+base64 --decode /tmp/CI_SSH > ~/.ssh/id_rsa
+base64 --decode /tmp/CI_SSH_PUB > ~/.ssh/id_rsa.pub
+echo "public key"
+cat ~/.ssh/id_rsa.pub
+# clean up
+rm  /tmp/CI_SSH
+rm  /tmp/CI_SSH_PUB
 
-# generate configuration of GitLab CI runner 
-RUN echo -e "concurrent = 1\n\n[[runners]]\n  name = \"$CI_NAME\"\n  url = \"$CI_URL\"\n  token = \"$CI_TOKEN\"\n  limit = 1\n  exectuor = \"shell\"" > /config.toml
+echo ""
+echo "configure runner"
+# generate configuration file
+echo "concurrent = 1\n\n[[runners]]\n  name = \"$CI_NAME\"\n  url = \"$CI_URL\"\n  token = \"$CI_TOKEN\"\n  limit = 1\n  exectuor = \"shell\"" > /etc/gitlab-runner/config.toml
+cat /etc/gitlab-runner/config.toml
 
+echo ""
+echo "start loop"
 while :
 do
 	# check if gitlab runner is running
